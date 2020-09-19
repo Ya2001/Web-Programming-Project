30bc67be2abf834ca796f8ef02a9215e37f8f732
as472 <as472@hw.ac.uk>
2020-09-19 19:05:59 +0200

    Connection notification

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("a");
+var express = require("express");
+var app = express();
+var server = require('http').Server(app);
+
+app.get("/", function(reg,res) 
+{
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use("client", express.static(__dirname + "/client"));
+
+server.listen(8080);
+console.log("Server started");
+
+var io = require('socket.io')(server, {});
+io.sockets.on('connection', function(socket){
+	console.log("Someone connected.")
+});
