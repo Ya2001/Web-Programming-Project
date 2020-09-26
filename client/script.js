f58fad1039b6eb633521752813e5a18d8106279b
Laura <laura.schauer212@gmail.com>
2020-09-26 13:04:58 +0100

    automated obstacle creation

--- client/script.js
+++ client/script.js
@@ -1,24 +1,38 @@
-// the buttons
+// IDEA: when the game starts, obstacles are created (random size, colour & y-position) at a certain time interval
+// depending on the difficulty level the players are on
+// obstacles movement: css animation: .obstacle-animation class is added to every newly created obstacle
+
+// the start & stop buttons to control the game
 var startButton = document.querySelector(".startButton");
 var stopButton = document.querySelector(".stopButton");
-
-// the obstacle
-var obstacle = document.querySelector(".obstacle");
+// event listener for the startButton & stopButton: when clicked, the game will start/stop
+startButton.addEventListener("click", startGame);
+stopButton.addEventListener("click", stopGame);
 
 // variable for the level (the higher, the more obstacles)
-var level = 3;
+var level = 4;
 
 // array for the colours used in the game
 // used in createObstacle
 var colours = ["#ffa500", "#3cb371", "#6a5acd", "#ee82ee"];
 
+// global variable for window.setInterval: saves the intervalID from startGame() so it can be later 
+// removed in stopGame()
+var intervalID;
 
-// event listener for the startButton & stopButton: when clicked, the game will start/stop
-startButton.addEventListener("click", startGame);
-stopButton.addEventListener("click", stopGame);
 
+// stop the game: everything that needs to start when the stopButton is clicked
+function stopGame() {
+    var i;
+    obstacleArray = document.getElementsByClassName("obstacle");
+    for (i = 0; i < obstacleArray.length; i++) {
+        obstacleArray[i].parentNode.removeChild(obstacleArray[i]);
+    }
+    window.clearInterval(intervalID);
+}
 
 // start the game: everything that needs to start when the startButton is clicked
+// var interval: variable for the difficulty level to control the speed of the obstacle creation
 function startGame() {
     var interval;
     switch (level) {
@@ -28,12 +42,26 @@ function startGame() {
             interval = 4000; break;
         case 3:
             interval = 3000; break;
+        case 4:
+            interval = 2000; break;
+        case 5:
+            interval = 1000; break;
         default:
             stopGame();
             break;
     }
-    addObstacle();
 
+    // add obstacles at the correct speed (according to difficulty)
+    intervalID = window.setInterval(function () {
+        addObstacle();
+    }, interval);
+}
+
+function addObstacle() {
+    //create a new obstacle
+    createObstacle();
+
+    // add the CSS animation to every obstacle that is created
     obstacleArray = document.getElementsByClassName("obstacle");
     var i;
     for (i = 0; i < obstacleArray.length; i++) {
@@ -41,40 +69,26 @@ function startGame() {
     }
 }
 
-// stop the game: everything that needs to start when the stopButton is clicked
-function stopGame() {
-    obstacle.classList.remove("obstacle-animation");
-}
-
-// function to create obstacles
-function addObstacle(level) {
-    // random width, height, colour, posX & posY for the new obstacle
+// creates new obstacles
+function createObstacle() {
+    // random width, height, colour, posX (within limits) 
     var width = Math.floor((Math.random() * 30) + 10);
     var height = Math.floor((Math.random() * 10) + 10);
     var colour = Math.floor(Math.random() * colours.length);
     var posX = Math.floor(Math.random() * (90 - width));
 
-    // get a random number between 1 & 100: Math.floor((Math.random() * 100) + 1);
-    createObstacle(width, height, colours[colour], posX);
-
-}
-
-function createObstacle(width, height, colour, posX) {
-    // create a new obstacle 
     var obstacle = document.createElement("div");
     obstacle.className = "obstacle";
 
     // size, colour of the obstacle
     obstacle.style.width = width + "vw";
     obstacle.style.height = height + "vh";
-    obstacle.style.border = "5px solid " + colour;
+    obstacle.style.border = "5px solid " + colours[colour];
 
     // start position of the obstacle
     obstacle.style.position = "absolute";
     obstacle.style.left = posX + "vw";
     obstacle.style.top = -100 + "vh";
-
-    document.querySelector(".canvas").style.overflow = "hidden";
 
     // add the newly created obstacle to the canvas
     document.getElementsByClassName('canvas')[0].appendChild(obstacle);
