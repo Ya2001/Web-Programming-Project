6b6680a2b8cb00e5ef7db02a5c873236ed561e50
Akshay Venkataramana <srivats66@gmail.com>
2020-10-05 16:40:20 +0100

    Adding css and javascript files to separate the code

---
+++ client/script.js
@@ -0,0 +1,32 @@
+var character = document.getElementById("character");
+var interval;
+var both = 0;
+
+
+function moveLeft(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>0){
+        character.style.left = left - 3 + "px";
+    }
+}
+function moveRight(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left<380){
+        character.style.left = left + 3 + "px";
+    }
+}
+document.addEventListener("keydown", event => {
+    if(both==0){
+        both++;
+        if(event.keyCode===37){
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.keyCode===39){
+            interval = setInterval(moveRight, 1);
+        }
+    }
+});
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
+    both=0;
+});
