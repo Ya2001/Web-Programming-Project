331b2b539c6b2f4b7b523aedd742ab0be6bcae13
as472 <as472@hw.ac.uk>
2020-10-30 14:10:51 +0000

    Added database

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const Datastore = require("nedb");
+
+
 var app = express();
 app.listen(8080, () => console.log("Server on"));
 
@@ -8,10 +11,15 @@ app.use(express.static("client"));
 //max data receivable is 1mb (anti ddos)
 app.use(express.json({limit: "1mb"}));
 
+//database
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 //make can get requests
 app.post('/server', (request, response) => {
 	console.log("User authenticated.")
-	console.log(request.body);
+	const data = request.body;
+	database.insert(data);
 	response.json({
 		status: "success",
 		username: request.body.uname,
