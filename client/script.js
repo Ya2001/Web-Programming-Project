9d2a2624379ead0eef97d8fd8dc8b93bbe76d1c2
Laura <laura.schauer212@gmail.com>
2020-11-11 11:15:11 +0000

    changed the way obstacles are created

--- client/script.js
+++ client/script.js
@@ -1,95 +1,188 @@
-// IDEA: when the game starts, obstacles are created (random size, colour & y-position) at a certain time interval
-// depending on the difficulty level the players are on
-// obstacles movement: css animation: .obstacle-animation class is added to every newly created obstacle
+// variable to track if start or stop was last clicked
+var gameIsOn;
 
 // the start & stop buttons to control the game
 var startButton = document.querySelector(".startButton");
 var stopButton = document.querySelector(".stopButton");
 // event listener for the startButton & stopButton: when clicked, the game will start/stop
-startButton.addEventListener("click", startGame);
-stopButton.addEventListener("click", stopGame);
+startButton.addEventListener("click", start);
+stopButton.addEventListener("click", () => gameIsOn = false);
 
-// variable for the level (the higher, the more obstacles)
-var level = 4;
+// variables for the game
+var gameWidth = 95; //vw
+var gameHeight = 90; //vh
 
-// array for the colours used in the game
-// used in createObstacle
-var colours = ["#ffa500", "#3cb371", "#6a5acd", "#ee82ee"];
-
-// global variable for window.setInterval: saves the intervalID from startGame() so it can be later 
-// removed in stopGame()
+// global variable for window.setInterval: saves the intervalID from obstacles() so it can be later 
+// removed in stop()
 var intervalID;
 
+// variables for the character
+var character = document.getElementById("character");
+var charWidth = 5;
+var charHeight = 5;
+var charSize = charWidth * charHeight;
 
+// variables for the obstacle
+var obstacle = document.getElementById("obstacle");
+var width;
+var height;
+var colour;
+var posX;
+var circleOrRectangle;
+
+
+/* ******************************************* window listeners *********************************************** */
+/* store all key presses in an object and later check what we need
+   source: https://dev.to/martyhimmel/moving-a-sprite-sheet-character-with-javascript-3adg */
+var keyPresses = {};
+
+window.addEventListener('keydown', keyDownListener, false);
+function keyDownListener(event) {
+    keyPresses[event.key] = true;
+}
+window.addEventListener('keyup', keyUpListener, false);
+function keyUpListener(event) {
+    keyPresses[event.key] = false;
+}
+
+/* ******************************************* moving the character *********************************************** */
+const MOVEMENT_SPEED = 10;
+var positionX = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+
+
+/* *************************************************** GAME LOOP ************************************************** */
+// starts the game and obstacle loops
+function start() {
+    gameIsOn = true;
+    /* start obstacle creation */
+    obstacles();
+    /* start the game by calling the game loop once, and then keep on calling it inside the loop */
+    window.requestAnimationFrame(gameLoop);
+}
+
+function gameLoop() {
+    update();
+    draw(positionX);
+
+    /* the browser performs the callback function on its own time: the browser takes care of picking a suitable fps
+    (= frame per second) for the device it is running on (usually about 60fps). */
+    if (gameIsOn) {
+        window.requestAnimationFrame(gameLoop);
+    } else {
+        stop();
+        window.cancelAnimationFrame(gameLoop);
+    }
+}
+
+// update the variables
+function update() {
+    // size of 1vw in px (used in moveLeft() & moveRight())
+    var oneVW = document.documentElement.clientWidth / 100;
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+
+    if (keyPresses.ArrowLeft) {
+        if (left > 8) {
+            positionX = left - MOVEMENT_SPEED + "px";
+        }
+
+    } else if (keyPresses.ArrowRight) {
+        if (left < oneVW * (gameWidth - charWidth) - 5) {
+            positionX = left + MOVEMENT_SPEED + "px";
+        }
+    }
+    // collision();
+}
+
+// update the graphics
+function draw(positionX) {
+    character.style.left = positionX;
+}
+
+
+/* ********************************************** stop the game *********************************************** */
 // stop the game: everything that needs to start when the stopButton is clicked
-function stopGame() {
-    var i;
-    obstacleArray = document.getElementsByClassName("obstacle");
-    for (i = 0; i < obstacleArray.length; i++) {
-        obstacleArray[i].parentNode.removeChild(obstacleArray[i]);
-    }
-    window.clearInterval(intervalID);
+function stop() {
+    // delete animation from obstacles
+    obstacle.style.top = (-1) * obstacle.style.height.replace(/[^0-9]/g, '') + "vw";
+    console.log(obstacle.style.top);
+    obstacle.classList.remove('obstacle-animation');
+
+    // put player back to starting position
+    character.style.left = 0;
+    positionX = 0;
 }
 
-// start the game: everything that needs to start when the startButton is clicked
-// var interval: variable for the difficulty level to control the speed of the obstacle creation
-function startGame() {
-    var interval;
-    switch (level) {
-        case 1:
-            interval = 5000; break;
-        case 2:
-            interval = 4000; break;
-        case 3:
-            interval = 3000; break;
-        case 4:
-            interval = 2000; break;
-        case 5:
-            interval = 1000; break;
-        default:
-            stopGame();
-            break;
-    }
 
-    // add obstacles at the correct speed (according to difficulty)
-    intervalID = window.setInterval(function () {
-        addObstacle();
-    }, interval);
+/* ********************************************** Obstacle creation *********************************************** */
+function obstacles() {
+    obstacle.classList.add('obstacle-animation'); // takes the CSS obstacle-animation and adds it to the obstacle div
+    obstacle.addEventListener('animationiteration', () => {
+        // change the position in every iteration
+        var random = Math.floor(Math.random() * gameWidth + 1);
+        width = obstacle.style.left.replace(/[^0-9]/g, ''); // the current width of the obstacle
+        obstacle.style.left = random - width + "vw"; // subtract width from obstacle so it doesn't go out of the canvas
+
+        // change the width and height in every iteration
+        var randomWidth = Math.floor(Math.random() * 10 + 1) // gives you a random number from 1 to 10
+        obstacle.style.width = randomWidth + "vw";
+        obstacle.style.height = Math.floor(Math.random() * 20 + 1) + "vw";
+    })
 }
 
-function addObstacle() {
-    //create a new obstacle
-    createObstacle();
+/* ********************************************** collision detection ********************************************** */
+function collision(object1, object2) {
+    // Checking if the player is colliding with a rectangle or a circle
+    if (circleOrRectangle == 0) {
+        return (
+            //This checks to see if the x and y coordinates of object 1 are touching or overlapping 
+            // the x and y coordinates of object 2 and returns a boolean value accordingly
+            (object1.left + object1.size / 2) >= (object2.left - object2.size / 2) &&
+            (object1.left - object1.size / 2) <= (object2.left + object2.size / 2) &&
+            (object1.top + object1.size / 2) >= (object2.top - object2.size / 2) &&
+            (object1.top - object1.size / 2) <= (object2.top + object2.size / 2)
+        )
+    }
 
-    // add the CSS animation to every obstacle that is created
-    obstacleArray = document.getElementsByClassName("obstacle");
-    var i;
-    for (i = 0; i < obstacleArray.length; i++) {
-        obstacleArray[i].classList.add("obstacle-animation");
+    else {
+        // Find the difference between the centre of the circle and the centre of the player character
+        var distX = Math.abs(object1.left - object2.left - object2.width / 2);
+        var distY = Math.abs(object1.top - object2.top - object2.height / 2);
+        // Check to see if the dictance between the centres is greater that the 
+        // radius of the circle + half the width of the rectangle since then they would be too far apart to touch
+        if (distX >= (object2.width / 2 + object1.width / 2)) { return false };
+        if (distY >= (object2.height / 2 + object1.width / 2)) { return false };
+        //If the distance is less than that then we can confirm that they are definately colliding
+        if (distX <= (object2.width / 2)) { return true };
+        if (distY <= (object2.height / 2)) { return true };
+        // We then use pythagoras theorum to compare the diztance from the centre of the rectangle
+        // to the centre of the circle. If this returns true then the rectangles corner is touching the circle
+        var dx = distX - object2.width / 2;
+        var dy = distY - object2.heigth / 2;
+        return (dx * dx + dy * dy <= (object2.width / 2) * (object2.width / 2));
     }
 }
 
-// creates new obstacles
-function createObstacle() {
-    // random width, height, colour, posX (within limits) 
-    var width = Math.floor((Math.random() * 30) + 10);
-    var height = Math.floor((Math.random() * 10) + 10);
-    var colour = Math.floor(Math.random() * colours.length);
-    var posX = Math.floor(Math.random() * (90 - width));
+function checkCollision() {
+    if (collision(obstacle, character)) {
+        gameOver("You got hit")
+    }
+}
+//Getting an error asking for a bracket when it is not necessary 
 
-    var obstacle = document.createElement("div");
-    obstacle.className = "obstacle";
-
-    // size, colour of the obstacle
-    obstacle.style.width = width + "vw";
-    obstacle.style.height = height + "vh";
-    obstacle.style.border = "5px solid " + colours[colour];
-
-    // start position of the obstacle
-    obstacle.style.position = "absolute";
-    obstacle.style.left = posX + "vw";
-    obstacle.style.top = -100 + "vh";
-
-    // add the newly created obstacle to the canvas
-    document.getElementsByClassName('canvas')[0].appendChild(obstacle);
+// Message to print when player hits an obstacle. This depends on whether they won or lost the game//
+function gameOver(message) {
+    gameIsOn = false;
+    setTimeout(printEndGameMessage(message, false));
+}
+function gameWin(message) {
+    gameIsOn = false;
+    setTimeout(printEndGameMessage(message, true));
+}
+function printEndGameMessage(message, gameWon) {
+    if (gameWon) {
+        alert(message);
+    }
+    else {
+        alert("Game Over \n" + message);
+    }
 }
