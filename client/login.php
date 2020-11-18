05fcf4f7ca3fd47ec2479dab47807063f06339ca
szelesaron <szelesaron39@gmail.com>
2020-11-18 12:26:10 +0000

    Added server.php and trying to make ajax work

---
+++ client/login.php
@@ -0,0 +1,55 @@
+<?php
+$error_login=''; //Variable to Store error message;
+$logged_in = false;
+
+if(isset($_POST['submit_login']))
+	{
+ 		if(empty($_POST['user']) || empty($_POST['pass']))
+		{
+ 			$error_login = "Username or Password is Invalid";
+ 		}
+ else
+ 	{
+		$conn = mysqli_connect("localhost", "root", "");
+
+		// Check connection
+		if (!$conn) 
+		{
+	  		die("Connection failed: " . mysqli_connect_error());
+		}
+
+		 //get the data - escape sql injection
+		 $user= mysqli_real_escape_string($conn, $_POST["user"]);
+		 $pass= $_POST["pass"];
+
+
+		 //Selecting Database
+		 $db = mysqli_select_db($conn, "users_db");
+		 //sql query to fetch information of registerd user and finds user match.
+
+		 //getting the hashed password from the database
+	 	 $hashed_query = mysqli_query($conn, "SELECT * FROM users WHERE  userName='$user'");
+
+	 	 $result = mysqli_fetch_assoc($hashed_query);
+	 	 $pwd_hash = $result['password'];
+
+		 if(password_verify($pass, $pwd_hash))
+		 {
+		 	$rows = mysqli_num_rows($hashed_query);
+
+		 }
+
+		 if($rows == 1)
+		 	{
+		 		 // Redirecting to other page
+		 		$logged_in = true;
+		 		header("Location: index.html?login_success");
+		 	}
+
+		 else
+			 {
+			 	$error_login = "Username or Password is Invalid";
+			 }
+		 mysqli_close($conn); // Closing connection
+	}
+}
