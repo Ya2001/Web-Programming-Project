1ee19b7c16639bc1824c15f4589453de04a1d7f0
szelesaron <szelesaron39@gmail.com>
2020-11-26 11:50:48 +0000

    Added all files

---
+++ client/error.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="error.css">
+    <title>Error in Registration</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="logo">
+            <h1>Drop Guys.</h1>
+        </div>
+    </div>
+
+    <div class="content-container">
+        <div class="content">
+            <h1>Sorry, that didn't work :(</h1>
+            <p>Please make sure that your passwords match and your username is as unique as possible.</p>
+            <div  class="controls">
+                <input class="login_again" type="submit" value="Login" name="login_again">
+                <script>
+                    var loginAgain = document.querySelector(".login_again");
+                    loginAgain.addEventListener("click", () => window.open("login_page.php", "_self"));
+                </script>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
